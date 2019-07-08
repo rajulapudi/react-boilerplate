2a059f8fedbffe9da83265647f24fe09d7ddc98f
praneeth <rajulapudip@gmail.com>
2019-07-08 11:12:43 +0530

    changed the default port to 5000

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ var htmlWebpackPlugin = require("html-webpack-plugin");
  
 module.exports = {
  entry: "./src/index.js",
+ devServer: {
+  contentBase: path.join(__dirname, 'dist'),
+  compress: true,
+  port: 5000
+},
  output: {
   path: path.resolve(__dirname, "dist"),
   filename: "index_bundle.js"
